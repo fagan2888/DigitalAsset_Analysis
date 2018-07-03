21528a9cfaaee35f89a1af520700687c7bdfc7af
Steve Lee <smlee.981@gmail.com>
2018-07-03 09:42:13 -0500

    read from aws rds instance

---
+++ code/twitterData_fromMysql_toCSV.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from sqlalchemy import create_engine
+import datetime
+import pandas as pd
+import config
+
+username = config.username
+password = config.password
+
+print("connecting...")
+engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
+print("reading...")
+df_aug = pd.read_sql('SELECT * FROM crypto_aug', con = engine)
+df_sept = pd.read_sql('SELECT * FROM crypto_sept', con = engine)
+df = df_aug.merge(df_sept, how='outer')
+print(df)
