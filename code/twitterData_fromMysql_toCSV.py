deb983fd42ab3f2f32921dfc16c23c21bb864b99
Steve Lee <smlee.981@gmail.com>
2018-07-16 21:34:38 -0500

    new dataset

--- code/twitterData_fromMysql_toCSV.py
+++ code/twitterData_fromMysql_toCSV.py
@@ -7,12 +7,21 @@ import config
 username = config.username
 password = config.password
 
+print("getting second batch...")
+
 print("connecting...")
 engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
-print("reading...")
-df_aug = pd.read_sql('SELECT * FROM crypto_aug', con = engine)
-df_sept = pd.read_sql('SELECT * FROM crypto_sept', con = engine)
-df = df_aug.merge(df_sept, how='outer')
 
+print("reading...")
+df = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date > "2017-12-07" and Date < "2017-12-15"', con = engine, chunksize=100000)
+#df = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date < "2017-12-07"', con = engine)
+
+'''
 print("writing to csv...")
-df.to_csv('twitter_crypto.csv')
+df.to_csv('twitter_bitcoin_two.csv')
+'''
+
+for chunk in df:
+    print("writing chunk to csv...")
+    chunkNoDuplicates = chunk.drop_duplicates()
+    chunkNoDuplicates.to_csv('twitter_bitcoin_two.csv', mode='a')
