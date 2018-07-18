6080214d78b654f28618302b0b86b9291effe20d
Steve Lee <smlee.981@gmail.com>
2018-07-18 00:09:52 -0500

    new dataset

--- code/twitterData_fromMysql_toCSV.py
+++ code/twitterData_fromMysql_toCSV.py
@@ -7,21 +7,23 @@ import config
 username = config.username
 password = config.password
 
-print("getting second batch...")
+print("getting all...")
 
 print("connecting...")
 engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
 
-print("reading...")
-df = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date > "2017-12-07" and Date < "2017-12-15"', con = engine, chunksize=100000)
-#df = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date < "2017-12-07"', con = engine)
+print("reading one...")
+dfOne = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date < "2017-12-07"', con = engine)
 
-'''
+print("reconnecting and reading two...")
+engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
+dfTwo = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date > "2017-12-07" and Date < "2017-12-11"', con = engine)
+
+print("reconnecting and reading three...")
+engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
+dfThree = pd.read_sql('SELECT * FROM bitcoin_dec WHERE Date > "2017-12-11" and Date < "2017-12-15"', con = engine)
+
+df = dfOne.merge(dfTwo.merge(dfThree, how='outer'), how='outer')
+
 print("writing to csv...")
-df.to_csv('twitter_bitcoin_two.csv')
-'''
-
-for chunk in df:
-    print("writing chunk to csv...")
-    chunkNoDuplicates = chunk.drop_duplicates()
-    chunkNoDuplicates.to_csv('twitter_bitcoin_two.csv', mode='a')
+df.to_csv('twitter_bitcoin_two.csv', chunksize=50000)
